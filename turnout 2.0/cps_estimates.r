3d245b5a54083b44c044af13a808ff0ebf51a943
Bailey Inglish <96551197+bailey-inglish@users.noreply.github.com>
2024-06-05 10:52:00 -0500

    meeting prep

--- turnout 2.0/cps_estimates.r
+++ turnout 2.0/cps_estimates.r
@@ -1,14 +1,18 @@
+# Libraries
 library(tidyverse)
 library(ipumsr)
 library(forcats)
 
+# Options
 setwd("C:/Users/baile/Desktop/asi/turnout 2.0")
 options(tibble.width = Inf)
 
+# Load data
 fips_conv <- read_csv("fips_name_abbr.csv")
 pums <- read_ipums_ddi("cps_00004.xml") %>% read_ipums_micro()
 turnout_actual <- read_csv("turnout_expanded.csv")
 
+# Clean data
 pums_clean <- filter(pums, (VOTED == 1 | VOTED == 2)) %>%
   left_join(
     fips_conv,
@@ -21,6 +25,7 @@ pums_clean <- filter(pums, (VOTED == 1 | VOTED == 2)) %>%
   ) %>%
   select(WTFINL, STATE, AGE, HISPAN, VOTED)
 
+# Compute summary leaderboards
 turnout_ovr <- group_by(pums_clean, STATE) %>%
   summarize(
     turnout_est = sum(VOTED * WTFINL) / sum(WTFINL),
@@ -47,8 +52,10 @@ turnout_hisp <- filter(pums_clean, HISPAN == TRUE) %>%
   ) %>%
   arrange(desc(turnout_est))
 
+# Join overall data to compare with actual for validity testing
 comp <- left_join(turnout_actual, turnout_ovr, by = c("state" = "STATE"))
 
+# VAP error
 ggplot(comp) +
   geom_col(
     aes(
@@ -62,5 +69,123 @@ ggplot(comp) +
       vjust = 0.5,
       hjust = 1
     )
+  ) +
+  labs(
+    title = "Error in voting age population",
+    subtitle = "For the November 2020 election",
+    x = "State",
+    y = "(Actual - calculated) as percent of actual (%)"
   )
 
+# Ballot error
+ggplot(comp) +
+  geom_col(
+    aes(
+      x = state,
+      y = 100 * (ballots - votes) / ballots
+    )
+  ) +
+  theme(
+    axis.text.x = element_text(
+      angle = 90,
+      vjust = 0.5,
+      hjust = 1
+    )
+  ) +
+  labs(
+    title = "Error in ballot/vote count",
+    subtitle = "For the November 2020 election",
+    x = "State",
+    y = "(Actual - calculated) as percent of actual (%)"
+  )
+
+# Raw turnout
+ggplot(turnout_ovr) +
+  geom_col(
+    aes(
+      x = fct_reorder(STATE, turnout_est),
+      y = turnout_est * 100
+    )
+  ) +
+  theme(
+    axis.text.x = element_text(
+      angle = 90,
+      vjust = 0.5,
+      hjust = 1
+    )
+  )  +
+  labs(
+    title = "Calculated Estimate of Voter Turnout",
+    subtitle = "For the November 2020 election",
+    x = "State",
+    y = "Voter turnout (%)",
+    caption = "Note: currently as a % of voting age population rather than VEP :("
+  )
+
+# Turnout error
+ggplot(comp) +
+  geom_col(
+    aes(
+      x = fct_reorder(state, (ballots / voting_age_pop) - turnout_est),
+      y = 100 * ((ballots / voting_age_pop) - turnout_est)
+    )
+  ) +
+  theme(
+    axis.text.x = element_text(
+      angle = 90,
+      vjust = 0.5,
+      hjust = 1
+    )
+  ) +
+  labs(
+    title = "Error in actual vs. CPS-expected turnout",
+    subtitle = "For the November 2020 Election",
+    x = "State",
+    y = "Actual - calculated (% pts)",
+    caption = "Note: Only using VAP rather than VEP for now :("
+  )
+
+# Young voters
+ggplot(turnout_young) +
+  geom_col(
+    aes(
+      x = fct_reorder(STATE, turnout_est),
+      y = turnout_est * 100
+    )
+  ) +
+  theme(
+    axis.text.x = element_text(
+      angle = 90,
+      vjust = 0.5,
+      hjust = 1
+    )
+  )  +
+  labs(
+    title = "Calculated Estimate of Voter Turnout",
+    subtitle = "For Voters Aged 18-24 the November 2020 Election",
+    x = "State",
+    y = "Voter turnout (%)",
+    caption = "Note: currently as a % of voting age population rather than VEP :("
+  )
+
+ggplot(turnout_hisp) +
+  geom_col(
+    aes(
+      x = fct_reorder(STATE, turnout_est),
+      y = turnout_est * 100
+    )
+  ) +
+  theme(
+    axis.text.x = element_text(
+      angle = 90,
+      vjust = 0.5,
+      hjust = 1
+    )
+  )  +
+  labs(
+    title = "Calculated Estimate of Voter Turnout",
+    subtitle = "For Hispanic/Latino Voters the November 2020 Election",
+    x = "State",
+    y = "Voter turnout (%)",
+    caption = "Note: currently as a % of voting age population rather than VEP :("
+  )
